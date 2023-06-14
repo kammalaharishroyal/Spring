fcd1147e6b02a4aae43c895f6254027213fde1bf
Harish <19691a0544@mits.ac.in>
2023-06-14 10:27:29 +0530

    1st commit uploading all files

---
+++ l6intro/src/main/java/com/example/demo/L6introApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L6introApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello harish..");
+		SpringApplication.run(L6introApplication.class, args);
+	}
+
+}
